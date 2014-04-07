45ffe68f961ac88dadcdd37f24e3a22011342920
mteahan85 <mteahan@ku.edu>
2014-04-07 12:19:10 -0500

    Create SpellDatabase.java
    Creation and storage of Champion Spells

---
+++ src/com/fourfoureight/lolhelper/General_Info/SpellDatabase.java
@@ -0,0 +1,22 @@
+public class SpellDatabase {
+
+	
+	public SpellDatabase()
+	{
+		
+		//create champion array
+		//store spell array of # spells for that champion in champion array, for easy access
+		//still create spells individually
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
