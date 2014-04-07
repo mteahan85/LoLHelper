1a37dc26802b31edcdde8035b3c1f28321642b29
mteahan85 <mteahan@ku.edu>
2014-04-07 12:17:36 -0500

    Create ChampionDatabase.java
    Creation and storage of Champions

---
+++ src/com/fourfoureight/lolhelper/General_Info/ChampionDatabase.java
@@ -0,0 +1,29 @@
+
+public class ChampionDatabase {
+
+	
+	public ChampionDatabase()
+	{
+		
+		ChampionInfo Kassadin = new ChampionInfo();
+		Kassadin.setNameTitle("Kassadin", "the Void Walker");
+		Kassadin.setTypes("Assassin", "Mage");
+		Kassadin.setInfo(3, 5, 8, 8);
+		Kassadin.setAttackDam(52.3,  3.9);
+		Kassadin.setAttackSpeed(0.64, 3.7);
+		Kassadin.setArmor(14, 3.2);
+		Kassadin.setRange(150, "Melee");
+		Kassadin.setMoveSpeed(340);
+		Kassadin.setHealthInfo(433,  78 , 6.95, 0.5);
+		Kassadin.setManaInfo(230, 45, 6.9, 0.6, 30, 0 );
+		//I need to add his spells later.
+		//also maybe have id number for him 
+		//giant array?
+		
+		
+		
+	}
+	
+	
+	
+}
